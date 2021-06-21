a525b7d793682cd217bb2122749d7bacad539485
MuhammadAli7896 <85828568+MuhammadAli7896@users.noreply.github.com>
2021-06-21 23:15:24 +0500

    JS Script Started

---
+++ Project_1/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password');
+
+form.addEventListener('submit',function(e) {
+    console.log('submit');
+})
