9c0ac20ebf07041415a581eefd1ef33d81485c84
MuhammadAli7896 <85828568+MuhammadAli7896@users.noreply.github.com>
2021-06-26 13:31:04 +0500

    Project 1 Java Done Project 1 Completed

--- Project_1/script.js
+++ Project_1/script.js
@@ -19,39 +19,59 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
-// Function to show success
-function isValidEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+// Function to check if email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ( re.test(input.value.trim()) ) {
+        showSuccess(input);
+    } else {
+        showError(input, `Please provide a valid email`)
+    }
+}
+
+// Function to check if required fields have data
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+        if ( input.value === '' ) {
+            console.log(input.id);
+            showError(input,`${getFieldId(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Function to check length of input field
+function checkLength(input, min, max) {
+    if ( input.value.length < min ) {
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Function to check if password and confirm password match
+function checkPasswordsMatch(input1, input2) {
+    if ( input1.value !== input2.value ) {
+        showError(input2,"Passwords don't match")
+    }
+}
+
+// Function to get the id of the input field with proper case
+function getFieldId(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // This is an event listener for the form on submit
 form.addEventListener('submit',function(e) {
     e.preventDefault();
-    
-    if ( username.value === '' ) {
-        showError(username,'Username is required')
-    } else {
-        showSuccess(username);
-    }
 
-    if ( email.value === '' ) {
-        showError(email,'Email is required')
-    } else if (!isValidEmail(email.value)) {
-        showError(email,'Email is invalid')
-    } else {
-        showSuccess(email);
-    }
+    checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
 
-    if ( password.value === '' ) {
-        showError(password,'Password is required')
-    } else {
-        showSuccess(password);
-    }
-
-    if ( password2.value === '' ) {
-        showError(password2,'Password2 is required')
-    } else {
-        showSuccess(password2);
-    }
 })
